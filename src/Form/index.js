423073cdb73a670e92734960a5d1a783e877becd
pawlo4-94 <pawlo4-94@o2.pl>
2021-07-16 19:50:19 +0200

    fetch in form edited

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,39 +1,47 @@
 import { StyledForm, StyledLabel, StyledInput, StyledField, StyledButton, StyledHeader } from "./styled";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import List from "./List";
 
-fetch("https://api.exchangerate.host/latest?base=PLN")
-    .then(response => response.json())
-    .then(console.log("Pobieranie danych..."))
-    .then(currency => {
-        setTimeout(() => {
-            console.log(`Waluta bazowa: ${currency.base}`);
-            console.log(`Data aktualizacji: ${currency.date}`);
-            for (const [key, value] of (Object.entries(currency.rates))) {
-                console.log(`${(value).toFixed(2)} ${key}`);
-            }
-        }, 1000);
-    }
-    )
-    .catch(error => console.error("Nie udało się pobrać danych...", error));
-
 const Form = ({ currencies }) => {
+    const [updateDate, setUpdateDate] = useState("");
+    const [sourceValue, setSourceValue] = useState("");
+    const [rates, setRates] = useState([]);
+    const [values, setValues] = useState([]);
+    useEffect(() => {
+        fetch("https://api.exchangerate.host/latest?base=PLN")
+            .then(response => response.json())
+            .then(console.log("Pobieranie danych..."))
+            .then(currency => {
+                setTimeout(() => {
+                    setRates(Object.keys(currency.rates));
+                    setValues(Object.values(currency.rates));
+                    setUpdateDate(currency.date);
+                    setSourceValue(currency.base);
+                }, 1000);
+            }
+            )
+            .catch(error => console.error("Nie udało się pobrać danych...", error))
+    }, []);
 
-    const initialValue = 0;
 
-    const [plnValue, setPlnValue] = useState(initialValue.toFixed(2));
+    const [plnValue, setPlnValue] = useState("");
     const [userValue, setUserValue] = useState("");
-    const [currencyValue, setCurrencyValue] = useState(currencies[0].value);
+    const [currencyValue, setCurrencyValue] = useState(values[0]);
 
-    const currencyNameIndex = currencies.findIndex(({ value }) => value === +currencyValue);
-    const currencyName = currencies[currencyNameIndex].name;
+    const currencyNameIndex = rates.findIndex(({ value }) => value === +currencyValue);
+    const currencyName = rates[currencyNameIndex];
 
-    const [result, setResult] = useState(plnValue);
+    const [result, setResult] = useState("");
+
+    const calculateResult = () => {
+        const currency = rates.find(({ name }) => name === currencyName).value;
+        setResult(userValue / rate);
+    };
 
     const onFormSubmit = event => {
         event.preventDefault();
         setPlnValue((+userValue).toFixed(2));
-        setResult((userValue / currencyValue).toFixed(2));
+        calculateResult();
         setUserValue("");
     };
 
@@ -63,30 +71,31 @@ const Form = ({ currencies }) => {
                         onChange={
                             ({ target }) => {
                                 setCurrencyValue(target.value);
-                                setPlnValue(initialValue.toFixed(2));
-                                setResult(initialValue.toFixed(2));
+                                setPlnValue("");
+                                setResult("");
                             }
                         }
                     >
-                        {(currencies.map(({ name, value, id }) => {
+                        {rates.map((rate, id) => {
                             return (
                                 <option
                                     key={id}
-                                    value={value}>
-                                    {name}
+                                    value={rate}>
+                                    {rate}
                                 </option>
                             )
-                        }))}
+                        })}
                     </StyledInput>
                 </StyledField>
                 <StyledButton>Przelicz</StyledButton>
             </StyledForm>
             <List
-                userValue={userValue}
                 plnValue={plnValue}
-                currencyValue={currencyValue}
-                currencyName={currencyName}
+                sourceValue={sourceValue}
+                currencyNameIndex={currencyNameIndex}
                 result={result}
+                rates={rates}
+                updateDate={updateDate}
             />
         </>
     );
