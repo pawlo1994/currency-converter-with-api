2493d18b05c73c165a823837ecc7166a2c4d8931
pawlo4-94 <pawlo4-94@o2.pl>
2021-07-17 00:46:25 +0200

    currencies array deleted

--- src/App.js
+++ src/App.js
@@ -9,31 +9,13 @@ import { ThemeProvider } from "styled-components";
 
 function App() {
 
-  const currencies = [
-    {
-      id: 1,
-      name: "EUR",
-      value: 4.47,
-    },
-    {
-      id: 2,
-      name: "USD",
-      value: 3.68,
-    },
-    {
-      id: 3,
-      name: "CHF",
-      value: 4.11,
-    },
-  ];
-
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <Header headerContent="suchy kantor" />
         <Clock />
         <Main>
-          <Form currencies={currencies} />
+          <Form />
         </Main>
         <Footer footerContent="Copyright &copy; Paweł Suchy 2021" />
       </Container>
