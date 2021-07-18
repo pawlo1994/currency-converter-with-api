9fa7e659817ababf6a3a91e322f2adf8fca7051f
pawlo4-94 <pawlo4-94@o2.pl>
2021-07-18 12:43:58 +0200

    if in list added

--- src/Form/List/index.js
+++ src/Form/List/index.js
@@ -1,6 +1,13 @@
 import { StyledHeader, StyledList, StyledParagraph } from "./styled";
 
 const List = ({ plnValue, result, updateDate, sourceValue }) => {
+    if (!result) {
+        return (
+            <StyledList>
+                <StyledHeader>Data aktualizacji: {updateDate}</StyledHeader>
+            </StyledList>
+        )
+    }
     return (
         <StyledList>
             <StyledHeader>Data aktualizacji: {updateDate}</StyledHeader>
