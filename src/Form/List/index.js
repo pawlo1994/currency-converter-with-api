4d03bc88ba72fbf3828ad1072262c0a13d73f69d
pawlo4-94 <pawlo4-94@o2.pl>
2021-07-18 23:10:58 +0200

    layout in list changed

--- src/Form/List/index.js
+++ src/Form/List/index.js
@@ -4,13 +4,15 @@ const List = ({ plnValue, result, updateDate }) => {
     if (!result) {
         return (
             <StyledList>
-                <StyledHeader>Data aktualizacji: {updateDate}</StyledHeader>
+                <StyledHeader>Data aktualizacji:</StyledHeader>
+                <StyledParagraph updateDateText>{updateDate}</StyledParagraph>
             </StyledList>
         )
     }
     return (
         <StyledList>
-            <StyledHeader>Data aktualizacji: {updateDate}</StyledHeader>
+            <StyledHeader>Data aktualizacji:</StyledHeader>
+            <StyledParagraph updateDateText>{updateDate}</StyledParagraph>
             <StyledParagraph>
                 {(+plnValue).toFixed(2)} PLN = {(+result.value).toFixed(2)} {result.currencyName}
             </StyledParagraph>
