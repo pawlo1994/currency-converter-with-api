7063f0ad39952682433d613376d4af4f7bf5a956
pawlo4-94 <pawlo4-94@o2.pl>
2021-07-22 16:20:12 +0200

    updateDate renamed into date

--- src/Form/List/index.js
+++ src/Form/List/index.js
@@ -1,18 +1,18 @@
 import { StyledHeader, StyledList, StyledParagraph } from "./styled";
 
-const List = ({ plnValue, result, updateDate }) => {
+const List = ({ plnValue, result, date }) => {
     if (!result) {
         return (
             <StyledList>
                 <StyledHeader>Data aktualizacji:</StyledHeader>
-                <StyledParagraph updateDateText>{updateDate}</StyledParagraph>
+                <StyledParagraph updateDateText>{date}</StyledParagraph>
             </StyledList>
         )
     }
     return (
         <StyledList>
             <StyledHeader>Data aktualizacji:</StyledHeader>
-            <StyledParagraph updateDateText>{updateDate}</StyledParagraph>
+            <StyledParagraph updateDateText>{date}</StyledParagraph>
             <StyledParagraph>
                 {(+plnValue).toFixed(2)} PLN = {(+result.value).toFixed(2)} {result.currencyName}
             </StyledParagraph>
